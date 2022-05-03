b0d5c8f2e21b67c6da066a3efa69af7449b53f55
Iasmina <il8465v@gre.ac.uk>
2022-05-03 10:47:25 -0700

    Environment finished + fog + changed player movement
    The other clowns need to be added

--- Installation Development Project/Assets/Scripts/ClownMovement.cs
+++ Installation Development Project/Assets/Scripts/ClownMovement.cs
@@ -7,32 +7,12 @@ public class ClownMovement : MonoBehaviour
 {
     [SerializeField] GameObject[] waypoints;
     NavMeshAgent NMAgent;
-    //Animator anim;
-
     [SerializeField] float speed;
-    //[SerializeField] float reachTargetRange;
-    //[SerializeField] int currentWaypoint = 0;
 
-
-    //float sightDistance = 9f;
-    //Vector3 raycastDirection;
-    //float currentSightAngle = 0f;
-    //float scanSpeed = 150f;
-    //float maxScanAngle = 45f;
-
-    //private bool throwPotion;
-    //public bool getThrowPotion
-    //{
-    //    get
-    //    {
-    //        return throwPotion;
-    //    }
-    //}
 
     void Start()
     {
         NMAgent = GetComponent<NavMeshAgent>();
-        //anim = GetComponent<Animator>(); - MAYBE NOT
 
         NMAgent.speed = speed;
         GoToWaypoint();
@@ -40,23 +20,11 @@ public class ClownMovement : MonoBehaviour
 
     void Update()
     {
-        //anim.SetFloat("MoveSpeed", NMAgent.velocity.magnitude * -2); -MAYBE NOT 
 
-        //if (NMAgent.remainingDistance < reachTargetRange)
-        //{
-        //    GoToWaypoint();
-        //}
-
-        //ScanForPlayer();
     }
 
     void GoToWaypoint()
     {
-        //currentWaypoint++;
-        //if (currentWaypoint >= waypoints.Length)
-        //{
-        //    currentWaypoint = 0;
-        //}
         NMAgent.SetDestination(waypoints[1].transform.position);
     }
 }
---
+++ Installation Development Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] GameObject[] waypoints;
+    NavMeshAgent NMAgent;
+    float speed = 0.2f;
+    float time;
+
+
+    void Start()
+    {
+        NMAgent = GetComponent<NavMeshAgent>();
+
+        NMAgent.speed = speed;
+        //NMAgent.acceleration = 0;
+    }
+
+    void Update()
+    {
+        //time = Time.deltaTime * speed * 0.2f;
+        //transform.position = Vector3.Lerp(transform.position, waypoints[1].transform.position, time);
+        
+        /*
+        if(Input.GetKey(KeyCode.P)) //Light sensor thing is off
+        {
+            //print("stopped");
+            NMAgent.destination = gameObject.transform.position;
+        }
+        else
+        {
+            //print("moving");
+            NMAgent.destination = waypoints[1].transform.position;
+        }*/
+
+        if (Input.GetKey(KeyCode.P)) //Light sensor thing is off
+        {
+            print("stopped");
+            transform.position = gameObject.transform.position;
+        }
+        else
+        {
+            print("moving");
+            time = Time.deltaTime * speed * 0.2f;
+            transform.position = Vector3.Lerp(transform.position, waypoints[1].transform.position, time);
+        }
+    }
+}
